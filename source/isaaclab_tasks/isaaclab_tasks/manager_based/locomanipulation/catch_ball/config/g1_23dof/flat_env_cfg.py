9ed5161e5b7c90737920cd4414ee60bc6a06fe3d
Fabio Amadio <fabioamadio93@gmail.com>
2025-02-10 14:25:15 +0100

    Adjusted base_height penalty weight

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomanipulation/catch_ball/config/g1_23dof/flat_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomanipulation/catch_ball/config/g1_23dof/flat_env_cfg.py
@@ -40,7 +40,7 @@ class G1CatchBallFlatEnvCfg(G1CatchBallRoughEnvCfg):
         self.rewards.feet_swing_height.params["target_height"] = 0.18
         self.rewards.feet_swing_height.weight = -1.0
         self.rewards.feet_slide.weight = -0.5
-        self.rewards.base_height_l2.weight = -0.5
+        self.rewards.base_height_l2.weight = -1.0
         self.rewards.dof_torques_l2.weight = -2.0e-6
         self.rewards.dof_torques_l2.params["asset_cfg"] = SceneEntityCfg(
             "robot", joint_names=[".*_hip_.*", ".*_knee_joint"]
@@ -74,7 +74,7 @@ class G1OnlyWalkFlatEnvCfg(G1OnlyWalkRoughEnvCfg):
         self.rewards.feet_swing_height.params["target_height"] = 0.18
         self.rewards.feet_swing_height.weight = -1.0
         self.rewards.feet_slide.weight = -0.5
-        self.rewards.base_height_l2.weight = -0.5
+        self.rewards.base_height_l2.weight = -1.0
         self.rewards.dof_torques_l2.weight = -2.0e-6
         self.rewards.dof_torques_l2.params["asset_cfg"] = SceneEntityCfg(
             "robot", joint_names=[".*_hip_.*", ".*_knee_joint"]
@@ -108,7 +108,7 @@ class G1StandingCatchBallFlatEnvCfg(G1StandingCatchBallRoughEnvCfg):
         self.rewards.feet_swing_height.params["target_height"] = 0.18
         self.rewards.feet_swing_height.weight = -1.0
         self.rewards.feet_slide.weight = -0.5
-        self.rewards.base_height_l2.weight = -0.5
+        self.rewards.base_height_l2.weight = -1.0
         self.rewards.dof_torques_l2.weight = -2.0e-6
         self.rewards.dof_torques_l2.params["asset_cfg"] = SceneEntityCfg(
             "robot", joint_names=[".*_hip_.*", ".*_knee_joint"]
